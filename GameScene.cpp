ed4db5d8ac7c8b95ec5352cab1b0fb61fba7293d
SoraSano <k021g1331@g.neec.ac.jp>
2022-10-21 13:09:51 +0900

    1章obj読み込み完成

--- GameScene.cpp
+++ GameScene.cpp
@@ -130,8 +130,8 @@ void GameScene::Draw()
 	/// <summary>
 	/// ここに前景スプライトの描画処理を追加できる
 	/// 
-	sprite1->Draw();
-	sprite2->Draw();
+	//sprite1->Draw();
+	//sprite2->Draw();
 	/// </summary>
 
 	// デバッグテキストの描画
